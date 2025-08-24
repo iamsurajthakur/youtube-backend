533936569f0eafa98b0ffc8966bce6cc6028c7d9
suraj <suraj.tech.in@gmail.com>
2025-08-24 15:05:06 +0545

    add custom API response and error handling

--- src/index.js
+++ src/index.js
@@ -6,25 +6,18 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>{
+    
+    app.on("error", (err)=> {
+        console.log("Error: ",err);
+        throw err
+    })
 
-
-/*
-import express from 'express'
-const app = express()
-
-( async () => {
-    try{
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        app.on("Error",(error)=>{
-            console.log("Error: ",error);
-            throw error
-        })
-        app.listen(process.env.PORT,()=>{
-            console.log(`Server is listenig at port ${process.env.PORT}`);
-        })
-    }catch(error){
-        console.error("ERROR: ",error)
-        throw error
-    }
-})()
-    */
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port ${process.env.PORT}`);
+        
+    })
+})
+.catch((err)=>{
+    console.log("mongoDB connection failed !!",err);
+})
