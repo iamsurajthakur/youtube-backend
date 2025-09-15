9590dbce21ca30080df9ebeb4380ae9fe45f02ae
suraj <suraj.tech.in@gmail.com>
2025-09-15 16:04:24 +0545

    fix bugs

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -246,7 +246,9 @@ const changeCurrentPassword = asyncHandler(async (req,res) => {
 const getCurrentUser = asyncHandler(async (req,res) => {
     return res
     .status(200)
-    .json(200, req.user, "current user fetched successfully.")
+    .json(
+        new ApiResponse(200, req.user, "Current user fetched.")
+    )
 })
 
 const updateAccountDetails = asyncHandler(async (req,res) => {
@@ -256,7 +258,7 @@ const updateAccountDetails = asyncHandler(async (req,res) => {
         throw new apiError(400,"All feild required")
     }
 
-    const user = User.findByIdAndUpdate(
+    const user = await User.findByIdAndUpdate(
         req.user?._id,
         {
             $set: {
