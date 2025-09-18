53266b9f9917e829dee5d7019d95db8aec6bb01d
suraj <suraj.tech.in@gmail.com>
2025-09-18 12:22:31 +0545

    fix bugs in user.routes.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -25,7 +25,7 @@ router.route('/change-password').post(verifyJWT, changeCurrentPassword)
 router.route('/current-user').get(verifyJWT, getCurrentUser)
 router.route('update-account').patch(verifyJWT, updateAccountDetails)
 router.route('/update-avatar').patch(verifyJWT, upload.single("avatar"), updateAvatar)
-router.route('/update-cover-image').patch(verifyJWT, upload.single("/coverImage"), updateCoverImage)
+router.route('/update-cover-image').patch(verifyJWT, upload.single("coverImage"), updateCoverImage)
 router.route('/c/:username').get(verifyJWT, getUserChannelProfile)
 router.route('/watch-history').get(verifyJWT, getWatchHistory)
 
